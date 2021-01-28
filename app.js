3181302ac1b1859ef4819b272ebd94a1e3479365
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2021-01-28 02:56:04 +0200

    app.js

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var reading = prompt ('Do you have interest in reading??');
 var number = prompt ('How many books do you read in the year??');
 var nmb;
 
-if (number > 10 && number < 20){
+if (number >= 10 && number < 20){
     nmb= "oh that is nice";
-} else if (number > 20 ){ 
+} else if (number >= 20 ){ 
     nmb= "You are a great reader!!";
 
-} else if (number < 10){
+} else if (number =< 10){
     nmb= "you can do better ";
 } 
 document.write(nmb);
 document.getElementById("good").innerText=number;
 
 confirm("you are in the right path");
-alert ("keep going");
+alert("keep going");
 
 
 
